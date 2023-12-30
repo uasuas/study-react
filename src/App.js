5d4da8303e4ab75a78ca0912b03a68a6489ea566
usausa <143808163+uasuas@users.noreply.github.com>
2023-12-30 15:37:26 +0900

    [update] インデント修正

--- src/App.js
+++ src/App.js
@@ -6,40 +6,39 @@ import TodoList from './TodoList';
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-const[todos, setTodos] = useState([]);
-// inputで入力された内容をuseRefで取得するinputタグがどれかを定義する
-const todoNameRef = useRef();
+  const[todos, setTodos] = useState([]);
+  // inputで入力された内容をuseRefで取得するinputタグがどれかを定義する
+  const todoNameRef = useRef();
 
-// todoの追加
-const handleAddTodo = () => {
-  const name = todoNameRef.current.value;
-  if(name === "") return;
-  setTodos((prevTodos) => {
-    // ""..."スプレッド構文
-    // ...prevTodos前のタスクに対して配列で括り","で区切って、新タスク{ id: "1", name: name, completed: false }を追加していく
-    // uuidにてランダムなIDを振りたい箇所に、uuidv4()と記述
-    return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
-  })
-  todoNameRef.current.value = null;
-};
+  // todoの追加
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+    if(name === "") return;
+    setTodos((prevTodos) => {
+      // ""..."スプレッド構文
+      // ...prevTodos前のタスクに対して配列で括り","で区切って、新タスク{ id: "1", name: name, completed: false }を追加していく
+      // uuidにてランダムなIDを振りたい箇所に、uuidv4()と記述
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
+    })
+    todoNameRef.current.value = null;
+  };
 
-// 更新したいtodoを選択（ID）
-const toggleTodo = (id) => {
-  // 直接値を変えるのは好ましくないため、const newTodos = [...todos]にてtodosをコピー
-  const newTodos = [...todos];
-  // 更新したいtodoが、今存在するtodosの中身の指定されたtodo.idと一致するものを探す
-  const todo = newTodos.find((todo) => todo.id === id);
-  // 一致したtodoのcheckboxのチェックを反転させる
-  todo.completed = !todo.completed;
-  // その内容を更新
-  setTodos(newTodos);
-};
-// 完了したものを削除
-const handleClear = () => {
-  const newTodos = todos.filter((todo) => !todo.completed);
-  setTodos(newTodos);
-};
-
+  // 更新したいtodoを選択（ID）
+  const toggleTodo = (id) => {
+    // 直接値を変えるのは好ましくないため、const newTodos = [...todos]にてtodosをコピー
+    const newTodos = [...todos];
+    // 更新したいtodoが、今存在するtodosの中身の指定されたtodo.idと一致するものを探す
+    const todo = newTodos.find((todo) => todo.id === id);
+    // 一致したtodoのcheckboxのチェックを反転させる
+    todo.completed = !todo.completed;
+    // その内容を更新
+    setTodos(newTodos);
+  };
+  // 完了したものを削除
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
 
   return (
     <div>
